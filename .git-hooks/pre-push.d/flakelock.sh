3fe847f99d86853fab38d32c6fd1cc6b8f15f123
DefectiveDev <250895707+DefectiveDev@users.noreply.github.com>
2026-02-02 22:10:18 -0500

    feat!(git-hook) should now automatically push flake lock updates

--- .git-hooks/pre-push.d/flakelock.sh
+++ .git-hooks/pre-push.d/flakelock.sh
@@ -3,4 +3,7 @@ cd $(git rev-parse --show-toplevel)
 find . -name "flake.nix" -execdir nix flake lock \;
 git add .
 git commit -m 'update(flake.locks)'
+git fetch origin
+git rebase
+git push --no-verify
 exit 0
